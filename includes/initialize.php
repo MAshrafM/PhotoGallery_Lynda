b33f2ebedca46f071aba4cdd43c816f5fe5c1d2a
MAshraf <ee.mashraf@gmail.com>
2017-07-21 12:14:04 +0300

    initialziers

---
+++ includes/initialize.php
@@ -0,0 +1,16 @@
+<?php 
+  // Define core paths
+  defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR); // define seperator
+  defined('SITE_ROOT') ? null : define('SITE_ROOT', 'D:'.DS.'Xamp'.DS.'htdocs'.DS.'Lynda'.DS.'photo_gallery'); // site core
+  defined('LIB_PATH') ? null : define('LIB_PATH', SITE_ROOT.DS.'includes'); // libaray path
+  // load config
+  require_once(LIB_PATH.DS."config.php");
+  // load basic functions
+  require_once(LIB_PATH.DS."functions.php");
+  //load core objects
+  require_once(LIB_PATH.DS."session.php");
+  require_once(LIB_PATH.DS."database.php");
+  // load db related classes
+  require_once(LIB_PATH.DS."user.php");
+  
+?>
--- public/index.php
+++ public/index.php
@@ -1,4 +1,3 @@
 <?php 
-  require_once("../includes/functions.php")
-  require_once("../includes/session.php");
+  require_once("../includes/initialize.php");
 ?>
