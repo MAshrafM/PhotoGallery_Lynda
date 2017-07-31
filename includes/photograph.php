7236dbddc36df08517e61cac5d26aebb45cd0325
MAshraf <ee.mashraf@gmail.com>
2017-07-31 17:15:25 +0300

    photograph class

---
+++ includes/photograph.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once(LIB_PATH.DS.'database.php');
+
+class Photograph extends DatabaseObject{
+  protected static $table_name = "photographs";
+  protected static $db_fields = array('id', 'filename', 'type', 'size', 'caption');
+  public $id;
+  public $filename;
+  public $type;
+  public $size;
+  public $caption;
+  
+}
+?>
