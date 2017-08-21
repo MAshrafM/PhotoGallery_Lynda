f34200400fe1c8ebf730b0e0fbc06438a7050ec1
MAshraf <ee.mashraf@gmail.com>
2017-08-21 08:31:29 +0300

    pagination view

--- public/index.php
+++ public/index.php
@@ -3,10 +3,15 @@
   
   $page = !empty($_GET["page"]) ? (int)$_GET["page"] : 1;
   $per_page = 6;
-  
   $total_count = Photograph::count_all();
+  //$photos = Photograph::find_all();
   
-  $photos = Photograph::find_all();
+  $pagination = new Pagination($page, $per_page, $total_count);
+  
+  $sql = "SELECT * FROM photographs ";
+  $sql .= "LIMIT {$per_page} ";
+  $sql .= "OFFSET {$pagination->offset()}";
+  $photos = Photograph::find_by_sql($sql);
 ?>
 <?php include_layout_template('header.php'); ?>
   <h2>Gallery</h2>
@@ -17,4 +22,28 @@
       </a>
     </div>
   <?php endforeach; ?>
+  <div id="pagination" style="clear: both;">
+    <?php 
+      if($pagination->total_pages() > 1){
+        if($pagination->has_previous_page()){
+          echo "<a href=\"index.php?page=";
+          echo $pagination->previous_page();
+          echo "\"&laquo; Previous</a> ";
+        }
+        for($i = 1; $i <= $pagination->total_pages(); $i++){
+          if($i == $page){
+            echo " <span class=\"selected\">{$i}</span> ";
+          }
+          else{
+            echo " <a href=\"index.php?page={$i}\">{$i}</a> ";
+          }
+        }
+        if($pagination->has_next_page()){
+          echo " <a href=\"index.php?page=";
+          echo $pagination->next_page();
+          echo "\"&raquo; Next</a> ";
+        }
+      }
+    ?>
+  </div>
 <?php include_layout_template('footer.php'); ?>
